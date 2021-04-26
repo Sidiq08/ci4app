abb9a1dc85a6a1224cd55fa37eb12b6b9750a537
Sidiq08 <m.aulia08@mail.unpas.ac.id>
2021-04-26 07:16:25 +0700

    backupDB video seeding ke 12

---
+++ DB/ci4.sql
@@ -0,0 +1,142 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 26, 2021 at 02:15 AM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ci4`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `komik`
+--
+
+CREATE TABLE `komik` (
+  `id` int(11) NOT NULL,
+  `judul` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `penulis` varchar(255) NOT NULL,
+  `penerbit` varchar(255) NOT NULL,
+  `sampul` varchar(255) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `komik`
+--
+
+INSERT INTO `komik` (`id`, `judul`, `slug`, `penulis`, `penerbit`, `sampul`, `created_at`, `updated_at`) VALUES
+(2, 'sidiq 2ssss', 'sidiq-2ssss', 'Muhammad Sidiq', 'SDQ-Ps', '1619371688_71e1620be08760bbf2c5.png', '2021-04-21 19:32:51', '2021-04-25 12:28:08'),
+(10, 'ssss', 'ssss', 'ss', 'sss', '1619366606_aaa5907592d0ed25eeba.jpeg', '2021-04-25 11:03:26', '2021-04-25 11:03:26'),
+(11, 'aaaaaaaaaa', 'aaaaaaaaaa', 'aaaaaaaaaaa', 'aaaaaaaaa', 'default.png', '2021-04-25 12:15:15', '2021-04-25 12:15:15');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `version` varchar(255) NOT NULL,
+  `class` varchar(255) NOT NULL,
+  `group` varchar(255) NOT NULL,
+  `namespace` varchar(255) NOT NULL,
+  `time` int(11) NOT NULL,
+  `batch` int(11) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `version`, `class`, `group`, `namespace`, `time`, `batch`) VALUES
+(1, '2021-04-25-230105', 'App\\Database\\Migrations\\Orang', 'default', 'App', 1619392000, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orang`
+--
+
+CREATE TABLE `orang` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `orang`
+--
+
+INSERT INTO `orang` (`id`, `nama`, `alamat`, `created_at`, `updated_at`) VALUES
+(1, 'Muhammad Sidiq', 'JL. ABD No.123', '2021-04-25 18:25:40', '2021-04-25 18:25:40'),
+(2, 'Muhammad Aulia ', 'JL. ACCC No.1223', '2021-04-25 18:25:40', '2021-04-25 18:25:40');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `komik`
+--
+ALTER TABLE `komik`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `orang`
+--
+ALTER TABLE `orang`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `komik`
+--
+ALTER TABLE `komik`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `orang`
+--
+ALTER TABLE `orang`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
